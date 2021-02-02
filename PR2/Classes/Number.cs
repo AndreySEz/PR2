ca74e6c884cf66dbbdd37ceb5082fb061bb6d7e6
Новиков Дмитрий <chelovek1214@gmail.com>
2021-02-02 18:17:23 +0500

    Добавьте файлы проекта.

---
+++ PR2/Classes/Number.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+
+namespace PR2
+{
+    public class Number
+    {
+        public int number { get; set; }
+        public Number(int number)
+        {
+            this.number = number;
+        }
+        public void Check()
+        {
+            int multiplication = 1;
+            int[] numbers = new int[number.ToString().Length];
+            int numberEx = number;
+            for (int x = 0; x < numbers.Length; x++)
+            {
+                numbers[x] = numberEx % 10;
+                numberEx = (numberEx - numberEx % 10)/10;
+                multiplication *= numbers[x];
+            }
+            if (multiplication * 3 == number) MessageBox.Show("Утроенное произведение цифр числа равно числу");
+            else MessageBox.Show("Утроенное произведение цифр числа не равно числу");
+        }
+    }
+}
---
+++ PR2/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PR2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void resButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Number number = new Number(Convert.ToInt32(numberBox.Text));
+                number.Check();
+            }
+            catch
+            {
+                MessageBox.Show("Некорректное значение");
+            }
+        }
+    }
+}
